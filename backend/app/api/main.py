b767aef034ad55d2959f6fbd49c247ec0d424ad5
NN <mima0000@niuniudeMacBook-Air.local>
2026-09-05 12:23:54 +0800

    Prepare backend for cloud deployment

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1|192\.168\.\d+\.\d+|10\.\d+\.\d+\.\d+|172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+)(:\d+)?",
+    allow_origin_regex=r"(https://theorldismagic-art\.github\.io|http://(localhost|127\.0\.0\.1|192\.168\.\d+\.\d+|10\.\d+\.\d+\.\d+|172\.(1[6-9]|2\d|3[0-1])\.\d+\.\d+)(:\d+)?)",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
